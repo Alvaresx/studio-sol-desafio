e7e91069a52ae90a017cb7d64edb01a811eef7b8
Mariana Alvares <mariana11areal@hotmail.com>
2022-04-28 09:26:59 -0300

    Estilizando Leds.js

--- src/components/Leds.js
+++ src/components/Leds.js
@@ -5,14 +5,14 @@ function Leds() {
   return (
     <>
       <Styled.ContainerSegments id="container_segments">
-        <div data-testid="div_segments">
-          <div id="segment-a" className="segment active"></div>
-          <div id="segment-b" className="segment active"></div>
-          <div id="segment-c" className="segment active"></div>
-          <div id="segment-d" className="segment active"></div>
-          <div id="segment-e" className="segment active"></div>
-          <div id="segment-f" className="segment active"></div>
-          <div id="segment-g" className="segment"></div>
+        <div>
+          <Styled.Segment className="segment_a active" />
+          <Styled.Segment className="segment_b active" />
+          <Styled.Segment className="segment_c active" />
+          <Styled.Segment className="segment_d active" />
+          <Styled.Segment className="segment_e active" />
+          <Styled.Segment className="segment_f active" />
+          <Styled.Segment className="segment_g not_active" />
         </div>
       </Styled.ContainerSegments>
     </>
